04bf1940abaf54f5f8e6653d8cf0aa680648be36
Marina Zharinova <phyrebirrd@gmail.com>
2024-02-27 09:39:19 +0500

    fix: unused import

--- scrapper/src/main/java/edu/java/service/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/service/LinkUpdaterScheduler.java
@@ -2,7 +2,6 @@ package edu.java.service;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
