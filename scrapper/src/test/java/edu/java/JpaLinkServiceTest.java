14e7b0cf036f1151d1a31f1ee184f0355a58ac6b
Marina Zharinova <phyrebirrd@gmail.com>
2024-03-25 10:09:49 +0500

    add: jpa updater test

---
+++ scrapper/src/test/java/edu/java/JpaLinkServiceTest.java
@@ -0,0 +1,52 @@
+package edu.java;
+
+import edu.java.service.jpa.JpaChatService;
+import edu.java.service.jpa.JpaLinkService;
+import java.net.URI;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = {JpaTestConfig.class})
+@Sql(value = "classpath:sql/put_chat.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+public class JpaLinkServiceTest extends IntegrationTest {
+    private final JpaLinkService linkService;
+
+    @Autowired
+    public JpaLinkServiceTest(JpaLinkService linkService) {
+        this.linkService = linkService;
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void addsAndFindsLink() {
+        var chatId = 10000L;
+        var uri = URI.create(
+            "https://stackoverflow.com/questions/68547843/bean-named-userservice-is-expected-to-be-of-type-demo-spring-orm-userservice");
+
+        var result = linkService.add(chatId, uri);
+        var listResult = linkService.listAll(chatId);
+
+        assertThat(result.getUri()).isEqualTo(uri);
+        assertThat(listResult.stream().toList().getFirst().getUri()).isEqualTo(uri);
+    }
+    @Test
+    @Transactional
+    @Rollback
+    void removesAndNotFindsLink(){
+        var chatId = 10000L;
+        var uri = URI.create(
+            "https://stackoverflow.com/questions/68547843/bean-named-userservice-is-expected-to-be-of-type-demo-spring-orm-userservice");
+        linkService.add(chatId, uri);
+        linkService.remove(chatId, uri);
+
+        var listResult = linkService.listAll(chatId);
+
+        assertThat(listResult.isEmpty()).isTrue();
+    }
+}
--- scrapper/src/test/java/edu/java/JpaTestConfig.java
+++ scrapper/src/test/java/edu/java/JpaTestConfig.java
@@ -2,12 +2,17 @@ package edu.java;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import edu.java.client.GitHubClient;
+import edu.java.client.StackOverflowClient;
 import edu.java.domain.jpa.JpaChatRepository;
+import edu.java.domain.jpa.JpaEventRepository;
 import edu.java.domain.jpa.JpaLinkDao;
 import edu.java.service.ChatService;
 import edu.java.service.LinkService;
+import edu.java.service.LinkUpdater;
 import edu.java.service.jpa.JpaChatService;
 import edu.java.service.jpa.JpaLinkService;
+import edu.java.service.jpa.JpaLinkUpdater;
 import jakarta.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
@@ -15,13 +20,20 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import reactor.core.publisher.Mono;
 import static edu.java.IntegrationTest.POSTGRES;
 
 @Configuration
@@ -76,6 +88,49 @@ public class JpaTestConfig {
         JpaChatRepository chatRepository
     ) {
         return new JpaChatService(chatRepository);
+    }
+    @Bean LinkUpdater linkUpdater(
+        JpaLinkDao linkDao,
+        JpaEventRepository eventRepository,
+        GitHubClient gitHubClient,
+        StackOverflowClient stackOverflowClient
+    ) {
+        return new JpaLinkUpdater(linkDao, eventRepository, gitHubClient, stackOverflowClient);
+    }
+    @Bean
+    StackOverflowClient stackOverflowClient() {
+        HttpServiceProxyFactory httpServiceProxyFactory =
+            HttpServiceProxyFactory
+                .builderFor(WebClientAdapter.create(webClient("http://localhost:8080")))
+                .build();
+        return httpServiceProxyFactory.createClient(StackOverflowClient.class);
+    }
+
+    @Bean
+    GitHubClient gitHubClient() {
+        HttpServiceProxyFactory httpServiceProxyFactory =
+            HttpServiceProxyFactory
+                .builderFor(WebClientAdapter.create(webClient("http://localhost:8080")))
+                .build();
+        return httpServiceProxyFactory
+            .createClient(GitHubClient.class);
+    }
+    @Bean
+    @Scope("prototype")
+    WebClient webClient(String baseUrl) {
+        return WebClient.builder()
+            .baseUrl(baseUrl)
+            .exchangeStrategies(ExchangeStrategies
+                .builder()
+                .codecs(codecs -> codecs
+                    .defaultCodecs()
+                    .maxInMemorySize(500 * 1024))
+                .build())
+            .defaultStatusHandler(
+                HttpStatusCode::isError,
+                clientResponse -> Mono.empty()
+            )
+            .build();
     }
 
 }
