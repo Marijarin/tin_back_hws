e283ccc756a89fc408a911e373e5e3fe4a0d5a9a
Marina Zharinova <phyrebirrd@gmail.com>
2024-04-01 12:17:54 +0500

    fix: test failure

--- scrapper/src/test/java/edu/java/unit/RateLimiterTest.java
+++ scrapper/src/test/java/edu/java/unit/RateLimiterTest.java
@@ -14,8 +14,6 @@ import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -53,41 +51,43 @@ public class RateLimiterTest {
         Bucket bucket = Bucket.builder().addLimit(bw).build();
         Mockito.when(rateLimiterService.resolveBucketByIp("123")).thenReturn(bucket);
         Mockito.when(chatService.findChatById(1)).thenReturn(new ChatDao(1L, OffsetDateTime.now(), List.of()));
+
         mvc.perform(get("/tg-chat/1").with(remoteAddr("123")))
             .andExpect(status().isOk());
-        mvc.perform(get("/tg-chat/1").with(remoteAddr("123"))).andExpect(status().is4xxClientError());
+        mvc.perform(get("/tg-chat/1").with(remoteAddr("123"))).andExpect(status().is(429));
     }
 
-//    @Test
-//    void returns429WhenManyRequestsToGetLink() throws Exception {
-//        Bandwidth bw = Bandwidth.classic(
-//            1, Refill.greedy(
-//                1,
-//                Duration.ofHours(1)
-//            ));
-//        Bucket bucket = Bucket.builder().addLimit(bw).build();
-//        Mockito.when(rateLimiterService.resolveBucketByIp("1234")).thenReturn(bucket);
-//        URI uri = URI.create("https://www.youtube.com/");
-//        Mockito.when(linkService.add(1, uri)).thenReturn(new LinkDao(1L, uri, uri.getHost()));
-//        Map<String, String> headers = Map.of("Tg-Chat-Id", "1");
-//            HttpHeaders httpHeaders = new HttpHeaders();
-//        httpHeaders.setAll(headers);
-//        mvc.perform(get("/links").with(remoteAddr("1234"))
-//                .header(Objects.requireNonNull(httpHeaders.getFirst("Tg-Chat-Id"))))
-//            .andExpect(status().isOk());
-//        mvc.perform(get("/links").with(remoteAddr("1234"))
-//                .header(httpHeaders.getFirst("Tg-Chat-Id")))
-//            .andExpect(status().is4xxClientError());
-//    }
-//
-//    private static RequestPostProcessor remoteAddr(final String remoteAddr) {
-//        return new RequestPostProcessor() {
-//            @Override
-//            public @NotNull MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
-//                request.setRemoteAddr(remoteAddr);
-//                return request;
-//            }
-//        };
-//    }
+    @Test
+    void returns429WhenManyRequestsToGetLink() throws Exception {
+        Bandwidth bw = Bandwidth.classic(
+            1, Refill.greedy(
+                1,
+                Duration.ofHours(1)
+            ));
+        Bucket bucket = Bucket.builder().addLimit(bw).build();
+        Mockito.when(rateLimiterService.resolveBucketByIp("1234")).thenReturn(bucket);
+        URI uri = URI.create("https://www.youtube.com/");
+        Mockito.when(linkService.add(1, uri)).thenReturn(new LinkDao(1L, uri, uri.getHost()));
+        Map<String, String> headers = Map.of("Tg-Chat-Id", "1");
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAll(headers);
+
+        mvc.perform(get("/links").with(remoteAddr("1234"))
+                .headers(httpHeaders))
+            .andExpect(status().is(404));
+        mvc.perform(get("/links").with(remoteAddr("1234"))
+                .headers(httpHeaders))
+            .andExpect(status().is(429));
+    }
+
+    private static RequestPostProcessor remoteAddr(final String remoteAddr) {
+        return new RequestPostProcessor() {
+            @Override
+            public @NotNull MockHttpServletRequest postProcessRequest(@NotNull MockHttpServletRequest request) {
+                request.setRemoteAddr(remoteAddr);
+                return request;
+            }
+        };
+    }
 
 }
