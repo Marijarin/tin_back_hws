e70af0e3ebaff3dae7a6142f2430c8543fcf74db
Marina Zharinova <phyrebirrd@gmail.com>
2024-04-05 16:00:31 +0500

    add: bot sends updates from kafka to user

--- bot/src/main/java/edu/java/bot/service/UpdateKafkaListener.java
+++ bot/src/main/java/edu/java/bot/service/UpdateKafkaListener.java
@@ -1,6 +1,7 @@
 package edu.java.bot.service;
 
 import edu.java.bot.controller.dto.LinkUpdate;
+import edu.java.bot.service.model.SendUpdate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
@@ -12,15 +13,29 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class UpdateKafkaListener {
 
+    private final PenBot penBot;
+
+    public UpdateKafkaListener(PenBot penBot) {
+        this.penBot = penBot;
+    }
+
+
     @KafkaListener(topics = "messages.string",
                    groupId = "bot",
                    containerFactory = "updateKafkaListenerContainerFactory",
                    concurrency = "1")
     public void listenUpdate(
-        @Payload LinkUpdate message,
+        @Payload LinkUpdate linkUpdate,
         @Header(KafkaHeaders.RECEIVED_KEY) String key,
         @Header(KafkaHeaders.RECEIVED_PARTITION) int partition
     ) {
-        log.info("Received Message from partition {} with key {}: {}", partition, key, message);
+        log.info("Received Message from partition {} with key {}: {}", partition, key, linkUpdate);
+        var sendUpdate = new SendUpdate(
+            linkUpdate.url(),
+            linkUpdate.description(),
+            linkUpdate.tgChatIds(),
+            linkUpdate.description());
+        penBot.processUpdateFromScrapper(sendUpdate);
+        log.info("Done!");
     }
 }
